13c6b4697db1d6bdaa91c5652122b76a7ff11bed
hector7178 <hjgrimanmarcano@gmail.com>
2025-08-01 09:59:28 -0400

    update

--- app/protected/dashboard/page.tsx
+++ app/protected/dashboard/page.tsx
@@ -1,5 +1,8 @@
-"use client"
 // @ts-nocheck
+/* eslint-disable */
+
+"use client"
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Progress } from "@/components/ui/progress";
