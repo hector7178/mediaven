1333f1f5b79dd34c5c694dc653a2ef494dc9ead9
hector7178 <hjgrimanmarcano@gmail.com>
2025-08-01 12:09:06 -0400

    update

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,6 +1,10 @@
 
 
+import { Button } from "@/components/ui/button";
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
 import {  SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 export default function ProtectedLayout({
   children,
 }: {
@@ -23,31 +27,31 @@ export default function ProtectedLayout({
 ]
   return (
     <main className="min-h-screen flex flex-col items-center">
-      <SidebarProvider >
-        <SidebarHeader>
-          <SidebarTrigger />
-        </SidebarHeader>
-       <SidebarGroup>
-          <SidebarGroupLabel>Navegación</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items?.map((item) => (
-                <SidebarMenuItem key={item?.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item?.url}>
-                      <span>{item?.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
+      <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5 relative ">
+        <Drawer  direction="left">
+  <DrawerTrigger><MenuIcon className="w-6 h=6"/></DrawerTrigger>
+  <DrawerContent>
+    <DrawerHeader>
+      <DrawerTitle>
+        Navegacion
+      </DrawerTitle>
+      <DrawerDescription></DrawerDescription>
+    </DrawerHeader>
+    <DrawerDescription>
+      {items?.map((d,i:number)=>{
+        return (<Link href={d?.url} className="flex flex-row gap-2">{d?.title}</Link>)
+      })}
+    </DrawerDescription>
+    <DrawerFooter>
+      <Button>Submit</Button>
+      <DrawerClose>
+        <Button variant="outline">Cancel</Button>
+      </DrawerClose>
+    </DrawerFooter>
+  </DrawerContent>
+</Drawer>
           {children}
         </div>
-    </SidebarProvider>
-        
     </main>
   );
 }
